8e7ccc9b945b312903730fcaa2913ef58bc1fcd6
KRZYSZTOFP1989 <62620232+KRZYSZTOFP1989@users.noreply.github.com>
2020-10-22 21:45:27 +0200

    Product Controller

--- src/main/java/com/kodilla/ecommerce/controller/ProductControler.java
+++ src/main/java/com/kodilla/ecommerce/controller/ProductControler.java
@@ -1,7 +1,6 @@
 package com.kodilla.ecommerce.controller;
 
 import com.kodilla.ecommerce.dto.ProductDto;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
