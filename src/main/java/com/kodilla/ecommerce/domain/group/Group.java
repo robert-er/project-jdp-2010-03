f41aa924e821d404e946551eac6389d59c2b9fb6
szymonkat <szymonkat@gmail.com>
2020-10-22 22:02:15 +0200

    JDP201003-2_alfa

--- src/main/java/com/kodilla/ecommerce/domain/group/Group.java
+++ src/main/java/com/kodilla/ecommerce/domain/group/Group.java
@@ -4,16 +4,21 @@ import com.kodilla.ecommerce.domain.Product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
+@Setter
 @NoArgsConstructor
-
+@AllArgsConstructor
 public class Group {
     private long id;
     private String name;
     private List<Product> products;
 
+    public Group(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
