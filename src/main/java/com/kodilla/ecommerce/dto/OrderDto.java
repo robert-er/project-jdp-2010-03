04cc7a4738be5c5b95260096e992cb13c81c65da
robert-er <robert.roza.pl@gmail.com>
2020-10-24 00:49:48 +0200

    JDP201003-4 OrderDto - remove id, modify OrderController

--- src/main/java/com/kodilla/ecommerce/dto/OrderDto.java
+++ src/main/java/com/kodilla/ecommerce/dto/OrderDto.java
@@ -2,15 +2,11 @@ package com.kodilla.ecommerce.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class OrderDto {
 
-    private Long id;
     private String name;
     private String description;
-
 }
