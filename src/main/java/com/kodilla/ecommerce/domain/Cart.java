519104119161844d25d9860a674f043d6be46ee2
robert-er <robert.roza.pl@gmail.com>
2020-10-24 00:14:28 +0200

    JDP201003-15 add Cart from jdp201003-14 and empty Product

--- src/main/java/com/kodilla/ecommerce/domain/Cart.java
+++ src/main/java/com/kodilla/ecommerce/domain/Cart.java
@@ -1,2 +1,29 @@
-package com.kodilla.ecommerce.domain;public class Cart {
+package com.kodilla.ecommerce.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "CARTS")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "cart",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    List<Product> products = new ArrayList<>();
 }
--- src/main/java/com/kodilla/ecommerce/domain/Product.java
+++ src/main/java/com/kodilla/ecommerce/domain/Product.java
@@ -1,2 +1,15 @@
-package com.kodilla.ecommerce.domain;public class Product {
+package com.kodilla.ecommerce.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "PRODUCTS")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
