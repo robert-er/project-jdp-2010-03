56535aa468dc61dc45146236aa3c754b88fbfef8
KRZYSZTOFP1989 <62620232+KRZYSZTOFP1989@users.noreply.github.com>
2020-10-24 18:50:22 +0200

    Product Controller - changes Product and ProductController classes

--- src/main/java/com/kodilla/ecommerce/domain/Product.java
+++ src/main/java/com/kodilla/ecommerce/domain/Product.java
@@ -11,23 +11,19 @@ import javax.persistence.*;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name = "product")
+@Entity(name = "products")
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(name = "name")
     private String title;
 
-    @Column(name = "price")
     private Long price;
 
-    @Column(name = "description")
     private String description;
 
-    @Column(name = "quantity")
     private Long quantity;
 
 }
