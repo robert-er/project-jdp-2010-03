25e71f39a125162ce15f72db1f440d42efd45a4f
Sentarion <mbalabox@gmail.com>
2020-10-28 22:58:15 +0100

    JDP201003-12 relations fix

--- src/main/java/com/kodilla/ecommerce/domain/Product.java
+++ src/main/java/com/kodilla/ecommerce/domain/Product.java
@@ -1,5 +1,7 @@
 package com.kodilla.ecommerce.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,20 +22,17 @@ public class Product {
     private String description;
     private Long quantity;
 
-    //trzeba odkomentować po dodaniu encji Cart
-    /*@ManyToOne
-    @JoinColumn(name = "CART_ID")
-    private Cart cart;*/
+    @ManyToMany(mappedBy = "products")
+    private List<Cart> carts;
 
-    //trzeba odkomentować po dodaniu encji Group
-    /*@ManyToOne
-    @JoinColumn(name = "GROUP_ID")
-    private Group group;*/
+    @JsonBackReference
+    @NotNull
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name="group_id")
+    private Group group;
 
-    //trzeba odkomentować po dodaniu encji Order
-    /*@ManyToOne
-    @JoinColumn(name = "ORDER_ID")
-    private Order order;*/
+    @ManyToMany(mappedBy = "products")
+    private List<Order> orders;
 
     public Product(String title, BigDecimal price, String description, Long quantity) {
         this.title = title;
