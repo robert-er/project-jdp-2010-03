435517c904392498d116f956fd428fea13ebd7ba
robert-er <robert.roza.pl@gmail.com>
2020-10-28 09:40:51 +0100

    JDP201003-15 User: modify relation to Order

--- src/main/java/com/kodilla/ecommerce/domain/Product.java
+++ src/main/java/com/kodilla/ecommerce/domain/Product.java
@@ -1,15 +1,15 @@
 package com.kodilla.ecommerce.domain;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.List;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
@@ -19,11 +19,20 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int quantity;
 
-    @JsonBackReference
-    @NotNull
-    @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name="cart_id")
-    private Cart cart;
+    @ManyToMany(mappedBy = "products")
+    private List<Cart> carts;
 
+    @ManyToMany(mappedBy = "products")
+    private List<Order> orders;
+
+//    @JsonBackReference
+//    @NotNull
+//    @ManyToOne(cascade = CascadeType.PERSIST)
+//    @JoinColumn(name="group_id")
+//    private Group group;
 }
--- src/main/java/com/kodilla/ecommerce/domain/User.java
+++ src/main/java/com/kodilla/ecommerce/domain/User.java
@@ -1,11 +1,13 @@
 package com.kodilla.ecommerce.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 
 @NoArgsConstructor
@@ -18,14 +20,25 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String nickname;
     private String name;
     private String surname;
     private String email;
     private LocalDateTime signUpDate;
+    private boolean isBlocked;
+    private String randomKey;
+    private LocalDateTime timeOfCreationRandomKey;
 
     @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "cart_id")
     private Cart cart;
+
+    @JsonManagedReference
+    @OneToMany(targetEntity = Order.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY)
+    private List<Order> orders;
 
     @Override
     public boolean equals(Object o) {
