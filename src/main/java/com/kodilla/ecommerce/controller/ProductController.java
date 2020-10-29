18cff8c79230a9e441fd3aca711e597fdc7cec44
robert-er <robert.roza.pl@gmail.com>
2020-10-29 20:11:51 +0100

    Copy ProductController from branch JDP201003-1

---
+++ src/main/java/com/kodilla/ecommerce/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.kodilla.ecommerce.controller;
+
+import com.kodilla.ecommerce.dto.ProductDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/v1/product")
+public class ProductController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProduct")
+    public ProductDto getProduct(Long productId) {
+        return new ProductDto(1L, "Product title", 125L, "Product description", 23L);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    public void deleteProduct(Long productId) {
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
+    public ProductDto updateProduct(ProductDto productDto) {
+        return new ProductDto(2L, "Product title - Edit", 123L, "Product description - Edit", 234L);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createProduct", consumes = APPLICATION_JSON_VALUE)
+    public void createProduct(ProductDto productDto) {
+
+    }
+
+}
