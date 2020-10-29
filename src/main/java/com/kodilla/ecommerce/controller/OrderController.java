7d37e7403cb286126bbfc9e2460431e5f5e75139
robert-er <robert.roza.pl@gmail.com>
2020-10-29 20:29:06 +0100

    copy OrderController from branch JDP201003-4

---
+++ src/main/java/com/kodilla/ecommerce/controller/OrderController.java
@@ -0,0 +1,43 @@
+package com.kodilla.ecommerce.controller;
+
+import com.kodilla.ecommerce.dto.OrderDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/order")
+public class OrderController {
+
+    @GetMapping("{id}")
+    public OrderDto getOrder(@PathVariable Long id) {
+        return new OrderDto(1L, "test name", "test description");
+    }
+
+    @GetMapping
+    public List<OrderDto> getAllOrders() {
+        List<OrderDto> orders = new ArrayList<>();
+        orders.add(new OrderDto(2L, "name1", "description1"));
+        orders.add(new OrderDto(3L, "name2", "description2"));
+        return orders;
+    }
+
+    @PostMapping
+    public Long addOrder(@RequestBody OrderDto orderDto) {
+        return 1L;
+    }
+
+    @DeleteMapping
+    public void deleteOrder(@RequestParam Long id) {
+
+    }
+
+    @PutMapping
+    public OrderDto updateOrder(@RequestParam Long id, @RequestBody OrderDto orderDto) {
+        return orderDto;
+    }
+}
