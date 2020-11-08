d3936114cf9983c91b61f8c023ef6e6478d82f8d
justynaWoj <j.wojciechowska2011@gmail.com>
2020-11-08 16:45:26 +0100

    fixes

--- src/main/java/com/kodilla/ecommerce/service/ProductService.java
+++ src/main/java/com/kodilla/ecommerce/service/ProductService.java
@@ -1,10 +1,6 @@
 package com.kodilla.ecommerce.service;
 
 import com.kodilla.ecommerce.domain.Product;
-import com.kodilla.ecommerce.exception.NotFoundException;
-import com.kodilla.ecommerce.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
