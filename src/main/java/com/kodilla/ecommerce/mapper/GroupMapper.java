7233daf921297ba1fa01be6742e94547aade3ea0
KRZYSZTOFP1989 <62620232+KRZYSZTOFP1989@users.noreply.github.com>
2020-11-10 20:16:49 +0100

    Commit VIII

--- src/main/java/com/kodilla/ecommerce/mapper/GroupMapper.java
+++ src/main/java/com/kodilla/ecommerce/mapper/GroupMapper.java
@@ -1,5 +1,4 @@
 package com.kodilla.ecommerce.mapper;
-<<<<<<< HEAD
 import com.kodilla.ecommerce.domain.Group;
 import com.kodilla.ecommerce.dto.GroupDto;
 import lombok.RequiredArgsConstructor;
@@ -39,24 +38,3 @@ public class GroupMapper {
     }
 
 }
-
-=======
-
-import com.kodilla.ecommerce.domain.Group;
-import com.kodilla.ecommerce.dto.GroupInCartItemDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-public class GroupMapper {
-
-    public GroupInCartItemDto mapToGroupInCartDto(Group group) {
-        GroupInCartItemDto groupInCartItemDto = new GroupInCartItemDto();
-        groupInCartItemDto.setId(group.getId());
-        groupInCartItemDto.setName(group.getName());
-        groupInCartItemDto.setDescription(group.getDescription());
-        return groupInCartItemDto;
-    }
-}
->>>>>>> origin/master
